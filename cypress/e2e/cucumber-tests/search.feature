f8e2b6ae2f8d1a90efa0cd4c23c1b71e6fb558e8
retxeeD <pedro.carvalho_lima@outlook.com>
2024-01-18 17:40:14 -0300

    web test impl

--- cypress/e2e/cucumber-tests/search.feature
+++ cypress/e2e/cucumber-tests/search.feature
@@ -1,5 +1,9 @@
 Feature: Google search
 
 Scenario: search keeggo on google successfully
-    Given I open google page
-    Then I type keeggo
+    Given I open automationexercise login page
+    And I type login data
+    When I click in products
+    And I search for a product in the search bar
+    And I put the product in my cart
+    Then I valid the product in my cart
--- cypress/e2e/cucumber-tests/search/searchSteps.js
+++ cypress/e2e/cucumber-tests/search/searchSteps.js
@@ -1,9 +1,27 @@
-import {Given, Then } from "cypress-cucumber-preprocessor/steps";
+import {And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
-Given ('I open google page', ()=>{
-    cy.visit("https://www.google.com/", {timeout: 3000});
+Given ('I open automationexercise login page', ()=>{
+    cy.visit("https://www.automationexercise.com/login");
 });
 
-Then ('I type keeggo', ()=>{
-    cy.get('#APjFqb').type('keeggo{enter}')
+And ('I type login data', ()=>{
+    cy.get('[data-qa="login-email"]').type('teste2021@teste.com.br')
+    cy.get('[data-qa="login-password"]').type('teste{enter}')
 });
+
+When ('I click in products', ()=>{
+    cy.contains('Products').click();
+});
+
+And ('I search for a product in the search bar', ()=>{
+    cy.get('#search_product').type('blue top{enter}');
+})
+
+And ('I put the product in my cart', ()=>{
+    cy.contains('Add to cart').click();
+})
+
+Then ('I valid the product in my cart', ()=>{
+    cy.contains('View Cart').click();
+    cy.contains('Blue Top').should('exist');
+})
